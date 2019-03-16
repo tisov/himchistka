df3352c6dfacbcdc14fcccb4622b779732a90e94
anuar <a.tilektessov@mail.ru>
2019-03-16 05:10:33 +0500

    some fix

--- himchistka-mebeli/wp-config.php
+++ himchistka-mebeli/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define( 'DB_NAME', 'id6759000_wp_himchistka' );
+define( 'DB_NAME', 'wp_himchistka-mebeli' );
 
 /** Имя пользователя MySQL */
-define( 'DB_USER', 'id6759000_admin123' );
+define( 'DB_USER', 'root' );
 
 /** Пароль к базе данных MySQL */
-define( 'DB_PASSWORD', 'admin123' );
+define( 'DB_PASSWORD', '' );
 
 /** Имя сервера MySQL */
 define( 'DB_HOST', 'localhost' );
